dbbad043d68f90badcf4b157b69b9c6d5e1084ab
Jehyeon Lee <dasom8899981@gmail.com>
2026-05-26 20:07:06 +0900

    Initial commit

---
+++ Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  jhKi
+//
+//  Created by LJh on 5/26/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
